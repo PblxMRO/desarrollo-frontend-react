b33ba41b980968c1bdca1b75e686b8e6890873b4
JPMRO <juan.pablo.mro@gmail.com>
2024-08-14 21:19:30 -0400

    Components

---
+++ src/screens/Default.jsx
@@ -0,0 +1,19 @@
+import Logo from './_components/Logo';
+import Description from './_components/Description';
+import OpenLink from '../components/OpenLink';
+
+const Default = () => {
+  return (
+        <>
+          <header className="App-header">
+                <Logo/>
+                <Description/>
+                <OpenLink title="Learn React" url="https://reactjs.org"/>
+                <OpenLink title="Abrir repositorio" url="https://github.com/PblxMRO/desarrollo-frontend-react"/>
+           </header>
+        </>
+    );
+
+};
+
+export default Default;
---
+++ src/screens/_components/Github.jsx
@@ -0,0 +1,16 @@
+const Github = () =>{
+    return(
+        <>
+        <a
+     className="App-link"
+     href="https://github.com/PblxMRO/desarrollo-frontend-react"
+     target="_blank"
+     rel="noopener noreferrer"
+   >
+     Ir al repositorio en Github
+   </a>
+   </>
+    );
+}
+
+export default Github;
